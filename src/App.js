c308576d74830a7813f64a3a1eb0eeaba3a645ff
raianazaman <raiana.salmin@gmail.com>
2018-12-15 13:43:56 -0500

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import data from "./data.json";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+state = {
+  data,
+  score: 0,
+  topScore: 0
+};
+
+componentDidMount() {
+  this.setState({ data: this.shuffleData(this.state.data) });
+}
+
+CorrectGuess = newData => {
+  const { topScore, score } = this.state;
+  const newScore = score + 1;
+  const newTopScore = newScore > topScore ? newScore : topScore;
+  this.setState({
+    data: this.shuffleData(newData),
+    score: newScore,
+    topScore: newTopScore
+  });
+};
+
+IncorrectGuess = data => {
+  this.setState({
+    data: this.resetData(data),
+    score: 0
+  });
+};
+
+resetData = data => {
+  const resetData = data.map(item => ({ ...item, clicked: false }));
+  return this.shuffleData(resetData);
+};
+
+shuffleData = data => {
+  let i = data.length - 1;
+  while (i > 0) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+    i--;
   }
+  return data;
+};
+
+handleItemClick = id => {
+  let guessedCorrectly = false;
+  const newData = this.state.data.map(item => {
+    const newItem = { ...item };
+    if (newItem.id === id) {
+      if (!newItem.clicked) {
+        newItem.clicked = true;
+        guessedCorrectly = true;
+      }
+    }
+    return newItem;
+  });
+  guessedCorrectly
+    ? this.CorrectGuess(newData)
+    : this.IncorrectGuess(newData);
+};
+
+render() {
+  return (
+    <div>
+      <Wrapper>
+      <Header score={this.state.score} topScore={this.state.topScore} />
+        {this.state.data.map(item => (
+          <Card
+            key={item.id}
+            id={item.id}
+            shake={!this.state.score && this.state.topScore}
+            handleClick={this.handleItemClick}
+            image={item.image}
+          />
+        ))}
+      </Wrapper>
+     
+    </div>
+  );
+}
 }
 
 export default App;
