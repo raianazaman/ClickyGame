133b312b792130dcb4d4dbb71eba5e6175142e3b
raianazaman <raiana.salmin@gmail.com>
2018-12-15 14:04:56 -0500

    Updates

---
+++ precache-manifest.5bd6629c0ef9ee6c844d0f0e6a880cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "858b3fe85537186340a4",
+    "url": "/ClickyGame/static/js/main.858b3fe8.chunk.js"
+  },
+  {
+    "revision": "73ef16698c69059fb705",
+    "url": "/ClickyGame/static/js/1.73ef1669.chunk.js"
+  },
+  {
+    "revision": "858b3fe85537186340a4",
+    "url": "/ClickyGame/static/css/main.7ddf5a7c.chunk.css"
+  },
+  {
+    "revision": "1f81bc87707ee08588f9e06bc2dc65f5",
+    "url": "/ClickyGame/index.html"
+  }
+];
